bfd561800ff87bcc9615055125ce7faa60f3d62a
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2021-07-29 22:30:55 +0200

    Fix undefined variable

--- src/Asset/Processor.php
+++ src/Asset/Processor.php
@@ -405,6 +405,7 @@ class Processor
         }
 
         $commandsStr = implode(' && ', $assetCommands);
+        $name = $asset->name();
         $this->io->writeVerboseComment("Found {$commandsStr} compilation script for '{$name}'.");
 
         return $assetCommands;
